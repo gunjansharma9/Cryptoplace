7b03905583fab292d074ca63f4deff300c80b037
gunjansharma9 <gunjansharmahapur@gmail.com>
2024-07-29 01:54:54 +0530

    Initial commit

---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+export const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+
+    const [allCoin,setAllCoin] = useState([]);
+    const [currency,setCurrency] = useState({
+        name:"usd",
+        symbol:"$"
+    })
+
+    const fetchAllCoin = async() => {
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-MtUzUY9BTAjkNPzJynY3DcJd'}
+          };
+          
+          fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    }
+
+    const contextValue = {
+
+    }
+
+    return (
+        <CoinContext.Provider value={contextValue}>
+            {props.children}
+        </CoinContext.Provider>
+    )
+}
+
+export default CoinContextProvider;
---
+++ src/pages/Coin/Coin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Coin.css"
+
+const Coin = () => {
+  return (
+    <div>
+      COIN
+    </div>
+  )
+}
+
+export default Coin
